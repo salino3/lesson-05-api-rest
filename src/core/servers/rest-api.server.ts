2559931376629a3141d65cb5c041c34e0588ec9b
salino3 <programacionflavio@gmail.com>
2024-02-01 12:19:07 +0100

    create envConstants

---
+++ src/core/servers/rest-api.server.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+import { envConstants } from "../constants/env.constants.js";
+
+
+export const createRestApiServer = () => {
+    const restApiServer = express();
+    restApiServer.use(express.json());
+    restApiServer.use(
+      cors({
+        methods: envConstants.CORS_METHODS,
+        origin: envConstants.CORS_ORIGIN,
+        credentials: true,
+      })
+    );
+    return restApiServer;
+};
+
+
